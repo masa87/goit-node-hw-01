88b3d0570d19aa7c439d5463cd364c659f358028
Tomasz Mazur <61616164+masa87@users.noreply.github.com>
2022-03-01 18:52:23 +0100

    function added to index.js

--- index.js
+++ index.js
@@ -1,34 +1,56 @@
-// const fs = require("fs");
-// const path = require("path");
 const argv = require("yargs").argv;
-// const readline = require("readline");
-const { listContact, getContactById, removeContact } = require("./contacts");
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} = require("./contacts");
 require("colors");
 
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout,
-// });
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
-      listContact();
-      // ...
+      listContacts();
       break;
-
     case "get":
-      // ... id
+      rl.question("Podaj id kontaktu do wyświetlenia: ".yellow, (value) => {
+        getContactById(value);
+        rl.close();
+      });
       break;
-
     case "add":
-      // ... name email phone
+      rl.question("Podaj imię: ".yellow, (name) => {
+        rl.question("Podaj email: ".yellow, (email) => {
+          rl.question("Podaj numer telefonu: ".yellow, (phone) => {
+            addContact(name, email, phone);
+            rl.close();
+          });
+        });
+      });
       break;
-
     case "remove":
-      // ... id
+      rl.question("Podaj id kontaktu do usunięcia: ".yellow, (value) => {
+        removeContact(value);
+        rl.close();
+      });
       break;
-
     default:
       console.warn("\x1B[31m Unknown action type!");
   }
